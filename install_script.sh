e4e7bca75ced1d517fd6dd9637d8292f44463946
Jonsson Yan <yz808@outlook.com>
2021-07-12 22:02:04 +0800

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -205,7 +205,7 @@ function installDocker() {
     fi
   cat >"/etc/docker/daemon.json" <<EOF
 {
- "registry-mirrors":["https://registry.cn-hangzhou.aliyuncs.com","https://docker.mirrors.ustc.edu.cn","https://docker.mirrors.ustc.edu.cn","https://hub-mirror.c.163.com"]
+ "registry-mirrors":["https://docker.mirrors.ustc.edu.cn","https://docker.mirrors.ustc.edu.cn","https://hub-mirror.c.163.com"]
 }
 EOF
     systemctl enable docker
