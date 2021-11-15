e178b32ba24266cfe3ae05c633161f2c8d327bb8
YanZhuangZhuang <yz808@outlook.com>
2021-11-15 14:45:14 +0800

    fix: install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -296,9 +296,8 @@ RUN chmod +x /trojan-panel
 ENTRYPOINT ["/trojan-panel"]
 EOF
 
-  docker build -t trojan-panel-server -f ${TROJAN_PANEL_SERVER_DATA} . \
-  && docker run -d --name trojan-panel-server --restart always \
-  -p 8888:8888 \
+  docker build -t trojan-panel-server ${TROJAN_PANEL_SERVER_DATA} \
+  && docker run -d --name trojan-panel-server --restart always -p 8888:8888 trojan-panel-server \
   && docker network connect trojan-panel-network trojan-panel-server
   if [[ $? -eq 0 ]]; then
     echoContent skyBlue "---> Trojan Panel后端安装完成"
