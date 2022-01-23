af50c1ebd1d4b21d9bc65e3c14db4f89e61b98b9
jonssonyan <yz808@outlook.com>
2022-01-23 19:10:24 +0800

    fix: bug

--- install_script.sh
+++ install_script.sh
@@ -269,7 +269,7 @@ function installDockerDebian(){
     curl \
     gnupg \
     lsb-release \
-    umzip
+    unzip
   curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
   echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
