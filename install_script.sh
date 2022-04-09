491e94a803498be42f204d20669b89cdf7dbc08b
jonssonyan <yz808@outlook.com>
2022-04-10 01:36:05 +0800

    fix: install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -382,8 +382,8 @@ function installTrojanPanel() {
 FROM golang:1.17
 WORKDIR ${TROJAN_PANEL_DATA}
 ADD trojan-panel trojan-panel
-RUN chmod +x ./trojan-panel
-ENTRYPOINT ["./trojan-panel -host=${mariadb_ip} -password=${mariadb_pas} -port=${mariadb_port}"]
+RUN chmod +x trojan-panel
+ENTRYPOINT ["trojan-panel","-host=${mariadb_ip}","-password=${mariadb_pas}","-port=${mariadb_port}"]
 EOF
 
     # 初始化数据库
