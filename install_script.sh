e601712b774eea5a3423db9ab0c9e15e3e046d22
jonssonyan <yz808@outlook.com>
2022-11-23 09:52:10 +0800

    fix: bug fixes

--- install_script.sh
+++ install_script.sh
@@ -919,11 +919,10 @@ failure_testing() {
     if [[ -n $(docker ps -a -q -f "name=^trojan-panel-caddy$") ]]; then
       if [[ -z $(docker ps -q -f "name=^trojan-panel-caddy$" -f "status=running") ]]; then
         echo_content red "---> Caddy TLS运行异常"
-      else
-        domain=$(cat "${DOMAIN_FILE}")
-        if [[ -z $(cat "${DOMAIN_FILE}") || ! -d "${CADDY_ACME}${domain}" || ! -f "${CADDY_ACME}${domain}/${domain}.crt" ]]; then
-          echo_content red "---> 证书申请异常，请尝试重启服务器将重新申请证书或者重新搭建选择自定义证书选项"
-        fi
+      fi
+      domain=$(cat "${DOMAIN_FILE}")
+      if [[ -z $(cat "${DOMAIN_FILE}") || ! -d "${CADDY_ACME}${domain}" || ! -f "${CADDY_ACME}${domain}/${domain}.crt" ]]; then
+        echo_content red "---> 证书申请异常，请尝试重启服务器将重新申请证书或者重新搭建选择自定义证书选项"
       fi
     fi
     if [[ -n $(docker ps -a -q -f "name=^trojan-panel-mariadb$") && -z $(docker ps -q -f "name=^trojan-panel-mariadb$" -f "status=running") ]]; then
