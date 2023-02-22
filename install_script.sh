882730a8ffb761cfa5f09c266bc1345786b195b4
jonssonyan <yz808@outlook.com>
2023-02-22 12:22:25 +0800

    fix: version

--- install_script.sh
+++ install_script.sh
@@ -80,7 +80,7 @@ init_var() {
   trojan_panel_current_version=""
   trojan_panel_latest_version="v2.0.3"
   trojan_panel_core_current_version=""
-  trojan_panel_core_latest_version="v2.0.2"
+  trojan_panel_core_latest_version="v2.0.3"
 
   # SQL
   sql_200="alter table \`system\` add template_config varchar(512) default '' not null comment '模板设置' after email_config;update \`system\` set template_config = \"{\\\"systemName\\\":\\\"Trojan Panel\\\"}\" where name = \"trojan-panel\";insert into \`casbin_rule\` values ('p','sysadmin','/api/nodeServer/nodeServerState','GET','','','');insert into \`casbin_rule\` values ('p','user','/api/node/selectNodeInfo','GET','','','');insert into \`casbin_rule\` values ('p','sysadmin','/api/node/selectNodeInfo','GET','','','');"
