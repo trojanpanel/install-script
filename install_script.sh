36ba38f6bd486e501b33769f7465f7226c6803fc
jonssonyan <yz808@outlook.com>
2023-06-22 22:36:58 +0800

    fix: v2.1.8

--- install_script.sh
+++ install_script.sh
@@ -1294,7 +1294,7 @@ port=8081' >>${trojan_panel_config_path}
   version_214_215=("v2.1.4")
   if [[ "${version_214_215[*]}" =~ "${trojan_panel_current_version}" ]]; then
     docker exec trojan-panel-mariadb mysql -h"${mariadb_ip}" -P"${mariadb_port}" -u"${mariadb_user}" -p"${mariadb_pas}" -Dtrojan_panel_db -e "${sql_215}" &>/dev/null &&
-          trojan_panel_current_version="v2.1.5"
+      trojan_panel_current_version="v2.1.5"
   fi
 
   echo_content skyBlue "---> Trojan Panel数据结构更新完成"
@@ -1810,16 +1810,15 @@ failure_testing() {
   else
     if [[ -n $(docker ps -a -q -f "name=^trojan-panel-caddy$") ]]; then
       if [[ -z $(docker ps -q -f "name=^trojan-panel-caddy$" -f "status=running") ]]; then
-        echo_content red "---> Caddy2运行异常 错误日志如下："
+        echo_content red "---> Caddy2运行异常 运行日志如下："
         docker logs trojan-panel-caddy
       fi
       domain=$(cat "${DOMAIN_FILE}")
-      if [[ -z ${domain} || ! -d "${CERT_PATH}" || ! -f "${CERT_PATH}${domain}.crt" ]]; then
-        echo_content red "---> 证书申请异常，请尝试 1.换个子域名重新搭建 2.重启服务器将重新申请证书 3.重新搭建选择自定义证书选项 日志如下："
+      if [[ -n ${domain} && ! -f "${CERT_PATH}${domain}.crt" ]]; then
+        echo_content red "---> 证书申请异常，请尝试 1.换个子域名重新搭建 2.重启服务器将重新申请证书 3.重新搭建选择自定义证书选项"
         if [[ -f ${CADDY_LOG}error.log ]]; then
+          echo_content red "Caddy2错误日志如下："
           tail -n 20 ${CADDY_LOG}error.log | grep error
-        else
-          docker logs trojan-panel-caddy
         fi
       fi
     fi
